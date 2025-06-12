8816a1db3e8cdfc5075d3a1204866102376f4d93
Aguirre Mauricio <mauricioaguirre.elec@gmail.com>
2025-06-12 08:43:39 -0300

    Pequeñas correciones en el menú

--- liga-chad/src/main/java/com/info/servicios/gestorEquipo/EquipoService.java
+++ liga-chad/src/main/java/com/info/servicios/gestorEquipo/EquipoService.java
@@ -5,6 +5,6 @@ import com.info.dominio.Equipo;
 import java.util.List;
 
 public interface EquipoService {
-    Equipo crearEquipo(String nombre);
+    Equipo crearEquipo();
     List<Equipo> getEquipos();
 }
--- liga-chad/src/main/java/com/info/servicios/gestorEquipo/impl/EquipoServiceImpl.java
+++ liga-chad/src/main/java/com/info/servicios/gestorEquipo/impl/EquipoServiceImpl.java
@@ -1,18 +1,27 @@
 package com.info.servicios.gestorEquipo.impl;
 
 import com.info.dominio.Equipo;
+import com.info.entradautils.CreadorDeEquiposUtil;
 import com.info.servicios.gestorEquipo.EquipoService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class EquipoServiceImpl implements EquipoService {
-    public List<Equipo> equipos = new ArrayList<>();
+    private List<Equipo> equipos = new ArrayList<>();
+    private Scanner scanner;
+    private CreadorDeEquiposUtil creadorDeEquiposUtil;
+
+    public EquipoServiceImpl(Scanner scanner){
+        this.scanner = scanner;
+        this.creadorDeEquiposUtil = new CreadorDeEquiposUtil(scanner);
+    }
 
     @Override
-    public Equipo crearEquipo (String nombre){
-        Equipo equipo = new Equipo(nombre);
+    public Equipo crearEquipo (){
+        Equipo equipo = new Equipo(creadorDeEquiposUtil.crearEquipo());
         equipos.add(equipo);
         return equipo;
     }
