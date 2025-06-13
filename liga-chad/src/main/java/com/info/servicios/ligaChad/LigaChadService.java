e4b389362baa26846aff51ac0d7197871c00a1a2
Aguirre Mauricio <mauricioaguirre.elec@gmail.com>
2025-06-13 11:25:32 -0300

    Lista la funcionalidad de ver el promedio de gol por equipo

--- liga-chad/src/main/java/com/info/servicios/ligaChad/LigaChadService.java
+++ liga-chad/src/main/java/com/info/servicios/ligaChad/LigaChadService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface LigaChadService {
     void iniciarLiga();
     void goleadorDeLaLiga(List<Equipo> equipos);
+    void promedioGol(List<Equipo> equipos);
 }
--- liga-chad/src/main/java/com/info/servicios/ligaChad/impl/LigaChadServiceImpl.java
+++ liga-chad/src/main/java/com/info/servicios/ligaChad/impl/LigaChadServiceImpl.java
@@ -2,6 +2,7 @@ package com.info.servicios.ligaChad.impl;
 
 import com.info.dominio.Equipo;
 import com.info.dominio.Jugador;
+import com.info.dominio.Partido;
 import com.info.servicios.gestorEquipo.EquipoService;
 import com.info.servicios.gestorJugadores.JugadorService;
 import com.info.servicios.ligaChad.LigaChadService;
@@ -9,6 +10,7 @@ import com.info.servicios.seleccionadores.seleccionadorDeEquipos.SeleccionadorDe
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LigaChadServiceImpl implements LigaChadService {
@@ -37,6 +39,31 @@ public class LigaChadServiceImpl implements LigaChadService {
             }
         }
         System.out.println("El goleador es: " + goleador.getNombre() + " con " + goleador.getCantidadGoles() + " goles.");
+    }
+
+    @Override
+    public void promedioGol(List<Equipo> equipos){
+        if(equipos.isEmpty()){
+            System.out.println("No hay equipos.");
+            return;
+        }
+        for (Equipo equipo : equipos){
+            List<Partido> partidos = equipo.getPartidosJugados();
+            if(partidos.isEmpty()){
+                System.out.println(equipo.getNombre()+" no jugó ningún partido.");
+                continue;
+            }
+            int golesTotales = 0;
+            int partidosJugados = 0;
+
+            for(Partido partido : partidos){
+                Map<Equipo, Integer> resultado = partido.getResultado();
+                golesTotales += resultado.get(equipo);
+                partidosJugados++;
+            }
+            double promedio = (double) golesTotales / partidosJugados;
+            System.out.printf("El promedio de gol de %s es %.2f%n", equipo.getNombre(), promedio);
+        }
     }
 
 }
